a3377d4cd675336bfbcc93287452427c03a89562
m.irfan <m.irfanwebdev@gmail.com>
2026-06-09 20:02:27 +0700

    add preact integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), preact()]
 });
